8637431fc65f71f4431488495487d565ab3262e2
Christopher D. Walborn <tuirgin@gmail.com>
2017-03-13 14:43:03 -0400

    updated filegen to allow configurable length time+num expansion

--- filegen/filegen.rkt
+++ filegen/filegen.rkt
@@ -1,7 +1,8 @@
 #!/usr/bin/env racket
 #lang racket/base
 
-(require racket/cmdline
+(require gregor
+         racket/cmdline
          racket/file
          racket/format
          racket/list
@@ -16,6 +17,8 @@
 (define template-path (make-parameter #f))
 (define output-path (make-parameter "./filegen-output"))
 (define time (make-parameter (abs (current-milliseconds))))
+(define time-string (make-parameter (~t (now) "YYYYMMddHHmmssSSSSSSSSSSSSSSSSSSSSSSSSSS")))
+(define time+num-length (make-parameter 20))
 
 ;; TODO: Add option to start incrementing from a specified number.
 
@@ -28,6 +31,11 @@
     ("Default: 10"
      "Specify the number of files needed.")
     (count# (string->number file-count))]
+   [("--width" "-w")
+    width
+    ("Default: 20"
+     "Specify the character width of the @|time+num| value.")
+    (time+num-length (string->number width))]
    [("--output" "-o")
     output-directory
     ("Default: filegen-output"
@@ -43,9 +51,9 @@
  ---------------  ---------------------------------------------------------
  @|num|           Insert incremental number starting with \"1\".
 
- @|time+num|      Insert incremental number based on the current time in
-                  milliseconds. This allows for substitutions which will
-                  remain unique across multiple runs of filegen."
+ @|time+num|      Insert incremental number based on the current time.
+                  This allows for substitutions which will remain unique
+                  across multiple runs of filegen."
    ""
    #:args (template)
    (template-path template))
@@ -58,13 +66,15 @@
   (define count#-width (string-length (number->string (count#))))
   (for ([x (in-range (count#))])
     (define x-padded (~r (add1 x) #:min-width count#-width #:pad-string "0"))
-    (render/output (munge str x x-padded) x-padded)))
+    (render/output (munge str x-padded) x-padded)))
 
 (module+ test
   (test-case
       "Render: 1-wide"
-    (define in "MSH|^~\\&|SendApp@|num||SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|1234567890|P|2.6PID|1||PID-@|num|||LN-@|num|^FN@|num|^MN@|num|||19010101|")
-    (define out5 "MSH|^~\\&|SendApp5|SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|1234567890|P|2.6PID|1||PID-5||LN-5^FN5^MN5||19010101|")
+    (define in "MSH|^~\\&|SendApp@|num||SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|@|time+num||P|2.6PID|1||PID-@|num|||LN-@|num|^FN@|num|^MN@|num|||19010101|")
+    (define out5 "MSH|^~\\&|SendApp5|SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|2017031315|P|2.6PID|1||PID-5||LN-5^FN5^MN5||19010101|")
+    (time-string "2017031314065476781396500000000000000000")
+    (time+num-length 10)
     (output-path (~a "tmp-test-dir-render-" (current-seconds)))
     (template-path (~a "tmp-test-template-render-" (current-seconds) ".txt"))
     (count# 5)
@@ -76,13 +86,15 @@
      (render in)
      (directory-exists? (output-path))
      (file-exists? file-path)
-     (check-equal? out5 (file->string file-path))
+     (check-equal? (file->string file-path) out5)
      (delete-directory/files (path-only file-path))))
   (test-case
       "Render: 2-wide"
     (time 1487698079510)
     (define in "MSH|^~\\&|SendApp@|num||SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|@|time+num||P|2.6PID|1||PID-@|num|||LN-@|num|^FN@|num|^MN@|num|||19010101|")
-    (define out05 "MSH|^~\\&|SendApp05|SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|1487698079514|P|2.6PID|1||PID-05||LN-05^FN05^MN05||19010101|")
+    (define out05 "MSH|^~\\&|SendApp05|SendFac|RcvApp|RcvFac|20141103123456||ADT^A01^ADT_A01|201703131406547678139650000005|P|2.6PID|1||PID-05||LN-05^FN05^MN05||19010101|")
+    (time-string "2017031314065476781396500000000000000000")
+    (time+num-length 30)
     (output-path (~a "tmp-test-dir-render-" (current-seconds)))
     (template-path (~a "tmp-test-template-render-" (current-seconds) ".txt"))
     (count# 10)
@@ -94,25 +106,29 @@
      (render in)
      (directory-exists? (output-path))
      (file-exists? file-path)
-     (check-equal? out05 (file->string file-path))
+     (check-equal? (file->string file-path) out05)
      (delete-directory/files (path-only file-path)))))
 
-(define (munge str count count-string)
+(define (munge str count-string)
   (define (@num s)
     (string-replace s "@|num|" count-string))
   (define (@time+num s)
-    (string-replace s "@|time+num|" (number->string (+ (time) count))))
+    (string-replace s "@|time+num|"
+                    (~a (substring (time-string) 0 (- (time+num-length)
+                                                      (string-length count-string)))
+                        count-string)))
   (~> str @time+num @num))
 
 (module+ test
-  (test-equal? "Process template: empty" (munge "" 10 "10") "")
+  (test-equal? "Process template: empty" (munge "" "10") "")
   (test-equal? "Process template: multiple match"
-               (munge "^@|num|\r^^@|num|^^\b@|num|^" 1000 "1000")
+               (munge "^@|num|\r^^@|num|^^\b@|num|^" "1000")
                "^1000\r^^1000^^\b1000^")
-  (time 1487696956649)
-  (define time+5 1487696956654)
+  (time-string "201703131406547678139650000000000000000")
+  (time+num-length 25)
+  (define time+5 "2017031314065476781390005")
   (test-equal? "Process template: time+count"
-               (munge "@|num||@|time+num|" 5 "0005")
+               (munge "@|num||@|time+num|" "0005")
                (~a "0005|" time+5)))
 
 (define (render/output str count-string)
